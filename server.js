3f2ca8acd0a1f6888cd17bca71fd87acab97f59c
Koushik4 <srinivas.koushik4@gmail.com>
2021-07-22 19:41:00 +0530

    check

--- server.js
+++ server.js
@@ -1,23 +1,70 @@
 const express = require("express")
 const app = express()
+const url = require("url")
 const socketio = require("socket.io")
 const http = require("http")
 const path = require("path")
 const server = http.createServer(app)
 const io = socketio(server)
-const port =  process.env.PORT || 5000 ;
-let peerIds = []
+const port = process.env.PORT || 5000;
+
+let socketRooms = {}
+let roomId = 0
+let rooms = {}
 app.use(express.static(path.join(__dirname, "static")));
 app.get("/", (req, res) => {
-    return res.sendFile(path.join(__dirname, "static", "index.html"))
+    return res.sendFile(path.join(__dirname, "static", "welcome.html"))
+})
+app.post("/", (req, res) => {
+    let rId = roomId;
+    roomId++;
+    return res.redirect('/' + rId)
+})
+app.get("/:roomId", (req, res) => {
+    return res.sendFile(path.join(__dirname, "static", "video.html"))
 })
 io.on("connection", socket => {
-    socket.on("hi", (peerId) => {
-        socket.broadcast.emit("open",peerId);
+    socket.on("addNewUserToRoom", (peerId) => {
+        let roomId = url.parse(socket.handshake.headers.referer).pathname.substring(1);
+        socket.join(roomId);
+        socketRooms[socket.id] = [peerId,roomId]
+        if (rooms[roomId] == undefined) rooms[roomId] = [];
+        rooms[roomId].push(peerId);
+        console.log(socket.id,socket.rooms);
+        socket.emit("assignRoomId", roomId)
+        socket.to(roomId).emit("open", peerId);
     })
-    socket.on("hello",()=>{console.log("jdos")})
+    
+    socket.on("RemoveThisUser", (peerId, roomId) => {
+        console.log(socket.rooms)
+        removeUser(peerId, roomId)
+    })
 
+    socket.on("MuteThisUser",(roomId,peerId,mute)=>{
+        console.log("muteeee");
+        socket.to(roomId).emit("MuteThisUserFromServer",peerId,mute);
+    })
+    socket.on("VideoToggle",(roomId,id,videoOn)=>{
+        console.log("jsiojfdsofdsod");
+        socket.to(roomId).emit("VideoToggleFromServer",id,videoOn)
+    })
+    socket.on("disconnect", () => { 
+        console.log(socketRooms[socket.id]);
+        removeUser(socketRooms[socket.id][0],socketRooms[socket.id][1])
+    })
+
+    function removeUser(peerId,roomId) {
+        if (rooms[roomId] != undefined) {
+            console.log(rooms,peerId)
+            let index = rooms[roomId].indexOf(peerId);
+            rooms[roomId].splice(index);
+            if (rooms[roomId].length == 0) delete rooms[roomId];
+            console.log(socket.rooms)
+            socket.to(roomId).emit("RemoveThisUserFromServer", peerId)
+        }
+    }
 })
+
 server.listen(port, () => {
     console.log("Listening to 5000")
 })
--- static/js/app.js
+++ static/js/app.js
@@ -1,63 +1,109 @@
 const socket = io()
-const peer = new Peer({key: 'peerjs', host: 'mypeer1.herokuapp.com', secure: true, port: 443})
-
-console.log(peer)
+const peer = new Peer({ key: 'peerjs', host: 'mypeer1.herokuapp.com', secure: true, port: 443 })
+let isMute = true
+let videoOn = false
+let roomId = -1, id = -1;
 peer.on("open", peerId => {
-    console.log("fsinvsivsd");
+    id = peerId
+    console.log(socket.rooms);
     navigator.mediaDevices.getUserMedia({
         video: true,
         audio: true
     }).then(stream => {
         let video = document.createElement("video");
+        video.id = id
         video.srcObject = stream
         video.muted = true
         video.play()
         document.getElementById("koushik").appendChild(video)
     })
-    socket.emit("hi", peerId);
+    socket.on("assignRoomId", rId => {
+        roomId = rId
+    })
+    socket.on("RemoveThisUserFromServer", pId => {
+        document.getElementById(pId).remove()
+    })
+    socket.emit("addNewUserToRoom", peerId);
+    socket.on("MuteThisUserFromServer", (peerId, mute) => {
+        console.log(peerId, mute)
+        console.log(document.getElementById(peerId).muted)
+        document.getElementById(peerId).muted = mute
+    })
     socket.on("open", (id) => {
         console.log("people");
         navigator.mediaDevices.getUserMedia({
             video: true,
             audio: true
         }).then(stream => {
-            // console.log("Calling...", id, peerId)
-            console.log(peerId,id)
+            // console.log(peerId,id)
             let call = peer.call(id, stream);
             let video = document.createElement("video");
+            video.id = call.peer;
             call.on("stream", function (remoteStream) {
                 console.log("Calling")
-                addVideo(video,remoteStream)
+                addVideo(video, remoteStream)
             })
-            call.on('close',()=>{
-                video.remove()
+            call.on('close', () => {
+                endCall()
             })
             // addVideo(stream)
         })
-        console.log("helllo world");
     })
 
+    socket.on("VideoToggleFromServer", (id, videoOn) => {
+        console.log(videoOn);
+        if (!videoOn) {
+            console.log(document.getElementById(id))
+            document.getElementById(id).pause()
+        }
+        else{ document.getElementById(id).play()}
+
+    })
     peer.on("call", call => {
         navigator.mediaDevices.getUserMedia({
             video: true,
             audio: true
         }).then(stream => {
             call.answer(stream)
+            // console.log("Call is {from} to {to}",call.peer,peerId)
             let video = document.createElement("video");
+            video.id = call.peer;
             call.on("stream", function (remoteStream) {
-                console.log("answering")
-                addVideo(video,remoteStream)
+                addVideo(video, remoteStream)
+            })
+            call.on('close', () => {
+                endCall()
             })
         })
     })
+
+
 })
 
+socket.on("dis", (pId) => {
+    console.log(socket.rooms)
+    socket.emit("print", id);
+})
+function endCall() {
+    console.log("jis");
+    socket.emit("RemoveThisUser", id, roomId);
+    document.getElementById(id).remove();
+}
+function mute() {
+    isMute = !isMute
+    console.log(isMute)
+    socket.emit("MuteThisUser", roomId, id, isMute)
+}
 
-
-function addVideo(video,stream) {
-    console.log("kou")
-    
+function video() {
+    videoOn = !videoOn
+    socket.emit("VideoToggle", roomId, id, videoOn)
+}
+function addVideo(video, stream) {
     video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+      })
     video.play()
     document.getElementById("koushik").appendChild(video)
 }
